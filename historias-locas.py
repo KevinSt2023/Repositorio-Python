7306e38f2ff95cdbc28f3b53f98caa345c910351
Kevin <189381907+SistemasMGP@users.noreply.github.com>
2024-12-06 16:32:26 -0500

    Se realiza el primer commit para el proyecto

---
+++ historias-locas.py
@@ -0,0 +1,17 @@
+#Concatenar cadena de caracteres
+#Aprende a programar con _______
+
+# organizacion = "freeCodeCamp"
+
+# print("Aprende a programar con " + organizacion)
+# print("Aprende a programar con {}".format(organizacion))
+# print(f"Aprende a programar con {organizacion}")
+
+adj = input("Ingrese un adjetivo: ")
+verbo1 = input("Ingrese un primer verbo: ")
+verbo2 = input("Ingrese el segundo verbo: ")
+sustantivo_plural = input("Ingrese un sustantivo en plural: ")
+
+madlib = f"¡Programar es tan {adj}¡ siempre me emociona {verbo1} con freecodeCamp y asi poder {verbo2} mis {sustantivo_plural}"
+
+print(madlib)
